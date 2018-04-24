7e8dd58b7fd54e1c425a27d8fe928f28faaddc87
Samir Banjanovic <sbanj17@gmail.com>
2018-04-23 22:27:48 -0400

    Methods to do stuff
    writing all teh fun code to members that get to have all the fun

---
+++ src/Dumpede/CharacterCleanser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dumpede
+{
+    public static class CharacterCleanser
+    {
+        public static int ProcessInput(FileStream fs, StreamWriter sw, char[] newLineDelimiters = null, char[] itemsToClean = null)
+        {
+            newLineDelimiters = new[] { '\r', '\n' };
+            itemsToClean = new[] { '\"' };
+
+            // set default values for 
+            // processing variables
+            var index = 1;
+            var isEof = false;
+            var finalStatus = 0;
+            var toClean = false;
+            var error = false;
+            var c = '\0';
+
+            // set stream to beginning
+            fs.Seek(0, SeekOrigin.Begin);
+
+            // set initial state
+            var q = 0;
+
+            while (isEof == false)
+            {// begin reading the stream
+                c = (char)fs.ReadByte();
+
+                if ((q == 0 || q == 4) && c == '"')
+                {// opening quotes of item
+                    sw.Write(c);
+                    q = 1;
+                }
+                else if ((q == 1 || q == 3) && c == '"')
+                {// closing quote of item
+                    sw.Write(c);
+                    q = 2;
+                }
+                else if ((q == 1 || q == 3 || q == 4) && (toClean = itemsToClean.Contains(c)))
+                {// skip ignore values; replace new lines with space...prevents string concatination
+                    if (toClean)
+                        sw.Write(" ");
+
+                    q = 3;
+                }
+                else if (q == 2 && c == ',')
+                {// transition back to initial state
+                    sw.Write(c);
+                    q = 0;
+                }
+                else if (q == 2 && newLineDelimiters.Contains(c))
+                {// reached end of the line
+
+                    sw.Write(newLineDelimiters);
+
+                    q = 4;
+                    Console.WriteLine(++index);
+                }
+                else if (q == 1 || q == 2 || q == 3)
+                {// valid character write to file
+                    sw.Write(c);
+                    q = 1;
+                }
+                else if ((error = q == 0) || c == char.MaxValue)
+                {// exit reader...
+                    if (error)
+                        finalStatus = -1;
+                    else
+                        finalStatus = 1;
+
+                    isEof = true;
+                }
+            }// end while
+
+            return finalStatus;
+        }// end function    
+    }
+}
---
+++ src/Dumpede/SpaceCaser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dumpede
+{
+    public static class SpaceCaser
+    {
+        public static string CaseToSpace(string input)
+        {
+            char currentChar;
+            char nextChar;
+            bool canPeek = false;
+            int skip = 0;
+            int insertIndex = 0;
+            int inputLength = input.Length;
+
+            // double size assume, we have a space after each letter
+            char[] hooomanReadable = new char[inputLength * 2];
+
+            for (int i = 0; i < inputLength; i += skip)
+            {
+                currentChar = input[i];
+                canPeek = (i + 1) != inputLength;
+                nextChar = canPeek ? input[i + 1] : currentChar;
+
+                if (char.IsLower(currentChar) && (canPeek && char.IsUpper(nextChar)))
+                {
+                    hooomanReadable[insertIndex++] = currentChar;
+                    hooomanReadable[insertIndex++] = ' ';
+                    hooomanReadable[insertIndex++] = nextChar;
+                    skip = 2;
+                }
+                else if (char.IsUpper(currentChar) && i != 0 && (canPeek && char.IsLower(nextChar)))
+                {
+                    hooomanReadable[insertIndex++] = ' ';
+                    hooomanReadable[insertIndex++] = currentChar;
+                    hooomanReadable[insertIndex++] = nextChar;
+                    skip = 2;
+                }
+                else
+                {
+                    hooomanReadable[insertIndex++] = currentChar;
+                    skip = 1;
+                }
+            }
+
+            return new string(hooomanReadable).Substring(0, insertIndex);
+        }
+    }
+}
