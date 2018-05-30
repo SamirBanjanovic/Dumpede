a7edd9c7a76ba038d0b74dc26c539e4a858ffe7f
Samir Banjanovic <SamirBanjanovic@users.noreply.github.com>
2018-05-30 18:42:03 -0400

    added comments

--- src/Dumpede/UnCaser.cs
+++ src/Dumpede/UnCaser.cs
@@ -11,7 +11,8 @@ namespace Dumpede
             char currentChar;
             char nextChar;
             bool canPeek = false;
-            int skip = 0;
+            int skip = 0; // skip read based on number of characters we've copied
+                          // in this case it's either 1 or 2; pending our state
             int insertIndex = 0;
             int inputLength = input.Length;
 
@@ -25,21 +26,26 @@ namespace Dumpede
                 nextChar = canPeek ? input[i + 1] : currentChar;
 
                 if (char.IsLower(currentChar) && (canPeek && char.IsUpper(nextChar)))
-                {
+                {// found a qualifying space (eg: testCase) -> we are at "t" nextChar is "C"
+                 // insert separator
+                 // yields: [t|e|s|t| |C|~]
+                 //
                     hooomanReadable[insertIndex++] = currentChar;
                     hooomanReadable[insertIndex++] = separator;
                     hooomanReadable[insertIndex++] = nextChar;
                     skip = 2;
                 }
                 else if (char.IsUpper(currentChar) && i != 0 && (canPeek && char.IsLower(nextChar)))
-                {
+                {// we've entered state of continous upper case letters (eg: HTTPClient) and have arrived
+                 // at a position where the next char is lower. Thus, insert space before last upper case 
+                 // yields: [H|T|T|P| |C|l|~]
                     hooomanReadable[insertIndex++] = separator;
                     hooomanReadable[insertIndex++] = currentChar;
                     hooomanReadable[insertIndex++] = nextChar;
                     skip = 2;
                 }
                 else
-                {
+                { // we're in a continous lower or upper case state, keep copying character to new array                    
                     hooomanReadable[insertIndex++] = currentChar;
                     skip = 1;
                 }
